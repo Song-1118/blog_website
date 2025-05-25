1d0b0baaa54c60a0b5728e7f9c9d896b7ba594a9
Songziming <19924539884@163.com>
2025-05-25 17:02:59 +0800

    名称修复

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>主页</h1>
+  <el-menu>
+    
+  </el-menu>
+
   <router-view />
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
 const route = useRoute()
-console.log('当前路由信息:', route)
 </script>
